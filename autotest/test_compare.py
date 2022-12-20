73cb72541cdd0cd6f6237ceacd02553733d17642
Wes Bonelli <wpb3hw@virginia.edu>
2022-12-19 21:13:13 -0500

    feat(utils): move utils from modflow-devtools
    * head & budget file writing
    * disu creation helper
    * uniform flow field
    * comparison utils
    * namefile reading utility

---
+++ autotest/test_compare.py
@@ -0,0 +1,39 @@
+import pytest
+
+pytestmark = pytest.mark.skip(reason="todo")
+
+
+def test_calculate_diffmax():
+    pass
+
+
+def test_calculate_difftol():
+    pass
+
+
+def test_eval_bud_diff():
+    pass
+
+
+def test_compare_budget():
+    pass
+
+
+def test_compare_swrbudget():
+    pass
+
+
+def test_compare_heads():
+    pass
+
+
+def test_compare_concs():
+    pass
+
+
+def test_compare_stages():
+    pass
+
+
+def test_compare():
+    pass
