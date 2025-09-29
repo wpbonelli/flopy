2e83d2c889a6262f8896b93c2ce70593b3cba940
wpbonelli <wbonelli@ucar.edu>
2025-09-29 16:40:41 -0400

    chore(release): update version number

--- flopy/version.py
+++ flopy/version.py
@@ -1,4 +1,4 @@
 # flopy version file automatically created using
-# update_version.py on September 29, 2025 19:54:57
+# update_version.py on September 29, 2025 16:40:10
 
-__version__ = "3.9.4"
+__version__ = "3.10.0.dev4"
